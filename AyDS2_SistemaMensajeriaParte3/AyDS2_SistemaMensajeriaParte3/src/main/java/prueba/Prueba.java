2046358791e8062d6d720c0988648c8486ea163a
Onti289 <164356636+Onti289@users.noreply.github.com>
2025-05-10 17:44:19 -0300

    Inicio

---
+++ AyDS2_SistemaMensajeriaParte3/AyDS2_SistemaMensajeriaParte3/src/main/java/prueba/Prueba.java
@@ -0,0 +1,12 @@
+package prueba;
+
+public class Prueba {
+    public Prueba() {
+        super();
+    }
+
+    public static void main(String[] args) {
+        Prueba prueba = new Prueba();
+        System.out.println("Hola");
+    }
+}
